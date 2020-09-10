ec67c9da6a4ed722ee6d5cbddd875ffb6c910af5
Kayalin Van Kogelenberg <kayalin.van.kogelenberg@gmail.com>
2020-09-10 11:57:47 +0200

    notes on structure, random number generated, working on structuring if statements next

---
+++ script.js
@@ -0,0 +1,9 @@
+
+//will need a queryselector for the buttons as we want the pc to roll as soon as any of the buttons are clicked
+//random number gives a value between 1-100, 1-33= rock 34-66= paper 67-100= scissor
+//Change image depending on what pc rolls
+//compare the input of button vs pc roll
+// add win/lose/draw message
+let buttonsChoice= document.querySelectorAll("button")
+var randNumber= Math.floor(Math.random() * 100) + 1;
+
