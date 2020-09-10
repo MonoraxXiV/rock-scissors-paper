a7ce2a7ad4e16841058412c97c473235bbd5fd7f
Kayalin Van Kogelenberg <kayalin.van.kogelenberg@gmail.com>
2020-09-10 17:02:05 +0200

    buttons are listened to, shows opponents choice after you clicked your choice, still need to compare the both for results

--- script.js
+++ script.js
@@ -4,29 +4,57 @@
 //compare the input of button vs pc roll
 // add win/lose/draw message
 // figure out how to swap alt messages for inclusion
-let buttonsChoice = document.querySelectorAll("button")
-var randNumber = Math.floor(Math.random() * 100) + 1;
+
+
 var pcChoise;
 var replaceImage = document.createElement("img");
+var playerChoise;
+var randNumber = Math.floor(Math.random() * 100) + 1;
 
 console.log(randNumber);
 
-if (randNumber <= 33) {
-    pcChoise = "rock";
-    console.log("rock");
-    replaceImage.src = "./Resources/rock.png";
-    document.getElementById("target").src = replaceImage.src;
-    document.getElementById("target").alt = "image of a rock";
-} else if (randNumber > 34 && randNumber <= 66) {
-    pcChoise = "paper";
-    console.log("paper");
-    replaceImage.src = "./Resources/paper.png";
-    document.getElementById("target").src = replaceImage.src;
-    document.getElementById("target").alt = "image of a piece of paper";
-} else {
-    pcChoise = "scissors"
-    console.log("scissor");
-    replaceImage.src = "./Resources/scissor.png";
-     document.getElementById("target").src = replaceImage.src;
-    document.getElementById("target").alt = "image of a pair of scissors";
+
+document.getElementById("paper").addEventListener('click', function () {
+    playerChoise = "paper";
+    console.log("you chose paper");
+    pcTurn();
+})
+document.getElementById("rock").addEventListener('click', function () {
+    playerChoise = "rock";
+    console.log("you chose rock");
+    pcTurn();
+})
+document.getElementById("scissors").addEventListener('click', function () {
+    playerChoise = "scissors"
+    console.log("You chose scissors");
+    pcTurn();
+})
+
+function pcTurn() {
+
+    if (randNumber <= 33) {
+        pcChoise = "rock";
+        console.log("rock");
+        replaceImage.src = "./Resources/rock.png";
+        document.getElementById("target").src = replaceImage.src;
+        document.getElementById("target").alt = "image of a rock";
+        document.getElementById("result").innerHTML = "Your opponent chose: " + pcChoise;
+
+
+    } else if (randNumber > 34 && randNumber <= 66) {
+        pcChoise = "paper";
+        console.log("paper");
+        replaceImage.src = "./Resources/paper.png";
+        document.getElementById("target").src = replaceImage.src;
+        document.getElementById("target").alt = "image of a piece of paper";
+        document.getElementById("result").innerHTML = "Your opponent chose: " + pcChoise;
+    } else {
+        pcChoise = "scissors"
+        console.log("scissor");
+        replaceImage.src = "./Resources/scissor.png";
+        document.getElementById("target").src = replaceImage.src;
+        document.getElementById("target").alt = "image of a pair of scissors";
+        document.getElementById("result").innerHTML = "Your opponent chose: " + pcChoise;
+    }
 }
+
